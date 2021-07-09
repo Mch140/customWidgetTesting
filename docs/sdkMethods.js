9c6f6b34cac71eee0c321726710a1f528a63c923
Mch140 <75289572+Mch140@users.noreply.github.com>
2021-07-09 14:18:44 +0530

    Update sdkMethods.js

--- docs/sdkMethods.js
+++ docs/sdkMethods.js
@@ -1,49 +1,15 @@
 console.log(">>>>>>>>>> inside sdkMethod.js");
-errorMessage = document.getElementById("errorMessage");
-pathToData = "chatTranscript.lines";
-console.log(">>>>>>>>>> pathToData: "+ pathToData);
 
- 
-
-var updateCallback = function(data){
-    console.log(">>>>>>>>>> data in updateCallback: ")
-    console.log(JSON.stringify(data));
-    var path = data.key;
-    console.log(">>>>>>>>>> data.key in updateCallback: "+ path);
-    var value = data.newValue;
-    console.log(">>>>>>>>>> data.newValue in updateCallback: "+ value);
-    var line = value[value.length -1];
-    console.log(">>>>>>>>>> newValue[-1] in updateCallback: "+ line);
-    var movieName = line.text;
-    console.log("moviename : "+movieName);
-    if (line.source.toLowerCase()==="visitor"){
-        var url = "https://www.omdbapi.com?t="+movieName+"&apikey=fb7c45ff";
-        fetch(url)
-                .then(function(response){
-                    
-                    return response.json();
-                }
-                ).then(function(res){
-                    document.getElementById("Title").innerHTML = res.Title;
-
-                }).catch(function(error){
-                    console.log("I am inside catch");
-                    console.log("Error Message : "+error);
-                })
-    }
+var notificationHandler1 = function(data) {
+    // Do something with the notifications
+    console.log(">>>>>>>>>> @1111", data)
+    console.log(">>>>>>>>>> JSON@1111", JSON.stringify(data));
+};
+var notificationHandler2 = function(data) {
+    // Do something with the notifications
+    console.log(">>>>>>>>>> @2222", data)
+    console.log(">>>>>>>>>> JSON@2222", JSON.stringify(data));
 };
 
- 
-
-var notifyWhenDone = function(error) {
-    if (err){
-        console.log("I am inside notifyWhenDone function : "+err);
-    }
-    var pathToData = "chatTranscript.lines";
-    errorMessage.innerHTML = "Unable to find the movie";
-};
-
- 
-
-lpTag.agentSDK.init({});
-lpTag.agentSDK.bind(pathToData, updateCallback, notifyWhenDone);
+lpTag.agentSDK.init({notificationCallback: notificationHandler1});
+lpTag.agentSDK.onNotify(notificationHandler2);
