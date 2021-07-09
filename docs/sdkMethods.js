a6f93897bdd8cb1c99fa2acd8988a2e96af8afa2
Mch140 <75289572+Mch140@users.noreply.github.com>
2021-07-09 14:21:13 +0530

    Update sdkMethods.js

--- docs/sdkMethods.js
+++ docs/sdkMethods.js
@@ -4,11 +4,13 @@ var notificationHandler1 = function(data) {
     // Do something with the notifications
     console.log(">>>>>>>>>> @1111", data)
     console.log(">>>>>>>>>> JSON@1111", JSON.stringify(data));
+    document.getElementById("data1").innerHTML = JSON.stringify(data);
 };
 var notificationHandler2 = function(data) {
     // Do something with the notifications
     console.log(">>>>>>>>>> @2222", data)
     console.log(">>>>>>>>>> JSON@2222", JSON.stringify(data));
+    document.getElementById("data2").innerHTML = JSON.stringify(data);
 };
 
 lpTag.agentSDK.init({notificationCallback: notificationHandler1});
