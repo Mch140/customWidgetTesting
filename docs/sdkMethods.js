31e95983e3c5edfd4bc8e844d59c7faa4aab9200
Mch140 <75289572+Mch140@users.noreply.github.com>
2021-07-09 14:45:21 +0530

    Update sdkMethods.js

--- docs/sdkMethods.js
+++ docs/sdkMethods.js
@@ -1,27 +1,23 @@
 console.log(">>>>>>>>>> inside sdkMethod.js");
 
-{
-
-var notificationHandler1 = function(data) {
+var notificationHandler = function(data) {
     // Do something with the notifications
     console.log(">>>>>>>>>> 1111");
     console.log(">>>>>>>>>> @1111", data)
     console.log(">>>>>>>>>> JSON@1111", JSON.stringify(data));
     document.getElementById("data1").innerHTML = JSON.stringify(data);
 };
-
-}
-{
     
-var notificationHandler2 = function(data) {
+var notificationHandlerOnNotify = function(data) {
     // Do something with the notifications
     console.log(">>>>>>>>>> 2222");
     console.log(">>>>>>>>>> @2222", data)
     console.log(">>>>>>>>>> JSON@2222", JSON.stringify(data));
     document.getElementById("data2").innerHTML = JSON.stringify(data);
 };
-}
 
-lpTag.agentSDK.init({notificationCallback: notificationHandler1});
+lpTag.agentSDK.init({notificationCallback: notificationHandler});
 console.log(">>>>>>>>>> after INIT");
-lpTag.agentSDK.onNotify(notificationHandler2);
+lpTag.agentSDK.onNotify(notificationHandlerOnNotify);
+console.log(">>>>>>>>>> after OnNotify");
+
